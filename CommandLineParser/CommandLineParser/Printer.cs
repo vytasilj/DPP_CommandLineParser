72206d9f5d602157773bc653557476d7062bcb81
Jiří Vytasil <jiri.vytasil@gmail.com>
2015-04-01 11:27:57 +0200

    Pridani tridy Printer

---
+++ CommandLineParser/CommandLineParser/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLineParser.Options;
+
+namespace CommandLineParser
+{
+    public class Printer : IDisposable
+    {
+        private readonly TextWriter _writer;
+
+        public Printer(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+
+        public void WriteOptions(IEnumerable<IOption> options)
+        {
+            
+        }
+
+
+        public void Dispose()
+        {
+            if (_writer != null)
+            {
+                _writer.Close();
+                _writer.Dispose();
+            }
+        }
+    }
+}
